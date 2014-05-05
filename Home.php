a4c5232d9166de7e8c712e1ab01a06fe80ac08a5
RuslanDoel <RuslanDoel@users.noreply.github.com>
2014-05-05 09:42:02 +0700

    DOel
    Coba

---
+++ Home.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Ruslan_117006161</title>
+</head>
+<body bgcolor=#003399 text=#ffff00>
+<h1>TOKO KOMPUTER ISAKUIKI</h1>
+<h2>Toko kami menyediakan</h2>
+<h3>Komputer berbagai merek</h3>
+<h4>Appel, DELL, IBM, dll.</h4>
+<h3>CD-ROM dan CD-WRITER</h3>
+<h4>Asus, Lite On, Samsung.</h4>
+<h3>Asesoris Komputer</h3>
+<h4>USB Flash Disk, Hedset, Speaker, dll.</h4>
+</body>
+</html>
